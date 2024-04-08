471ff6c4534860e628e5c4073b2735e877ef9030
egzdeveloper <egzdeveloper@gmail.com>
2024-04-08 15:40:49 +0200

    [Fix] environments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { environment } from 'src/environments/environment';
   declarations: [AppComponent],
   imports: [
     MbscModule,
-    FormsModule,BrowserModule, IonicModule.forRoot(), AppRoutingModule, SharedModule, provideFirebaseApp(() => initializeApp({"projectId":"los-peines-de-ro-app","appId":"1:382168494715:web:cc00747ea5d0d2bafda8de","storageBucket":"los-peines-de-ro-app.appspot.com","apiKey":"AIzaSyBtBul50QAvw5SI1vqz6rCB5WSfU195Mws","authDomain":"los-peines-de-ro-app.firebaseapp.com","messagingSenderId":"382168494715"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
+    FormsModule,BrowserModule, IonicModule.forRoot(), AppRoutingModule, SharedModule, provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, { provide: FIREBASE_OPTIONS, useValue: environment.firebaseConfig }],
   bootstrap: [AppComponent],
 })
