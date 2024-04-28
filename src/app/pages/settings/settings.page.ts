9b490c1d54b61e6c432705b086c03cfb5af933ad
egzdeveloper <egzdeveloper@gmail.com>
2024-04-28 11:34:41 +0200

    [ADD] Scale setting

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { RangeCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss'],
 })
-export class SettingsPage implements OnInit {
+export class SettingsPage {
 
-  constructor() { }
+  constructor() {
 
-  ngOnInit() {
   }
 
+  changeScale(ev: Event) {
+    if ((ev as RangeCustomEvent).detail.value === 1) {
+      document.documentElement.style.fontSize = "0.7rem";
+    }
+  }
 }
